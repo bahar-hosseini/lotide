6abdc31933d0b83914172c20b6278caddfa0b504
vagrant <bahar.hosseini@gmail.com>
2022-07-27 00:27:49 +0000

    first commit for the eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  
+};
+
+const eqArrays = (arr1,arr2)=>{
+  if (!arr1 || !arr2) {
+    return false;
+  } else if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    return arr1.every((e,i)=> e === arr2[i]);
+  }
+};
+
+
+    
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+assertEqual(eqArrays('test', 'str'), false);
+
