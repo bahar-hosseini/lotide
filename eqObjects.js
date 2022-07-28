075c409479898f3d3dd7b55cbcf5ded3e4cfdcbf
vagrant <bahar.hosseini@gmail.com>
2022-07-28 17:36:30 +0000

    adding return

--- eqObjects.js
+++ eqObjects.js
@@ -25,7 +25,7 @@ const eqObjects = function(object1, object2) {
   }
   for (let key of keyObj1) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      eqArrays((object1[key]),(object2[key]));
+      return  eqArrays((object1[key]),(object2[key]));
     } else if (object1[key] === object2[key]) {
       return true;
     }
