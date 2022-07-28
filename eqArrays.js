060b4d98153f99c878a3596d8b74534c623b1383
vagrant <bahar.hosseini@gmail.com>
2022-07-28 17:16:38 +0000

    modifying the function

--- eqArrays.js
+++ eqArrays.js
@@ -4,15 +4,17 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1,arr2) {
-  if (!arr1 || !arr2) {
+
+  if (arr1.length !== arr2.length) {
     return false;
-  } else if (arr1.length !== arr2.length) {
-    return false;
-  } else if (arr1.length === 0 || arr2.length === 0) {
-    return false;
-  } else {
-    return arr1.every((e,i)=> e === arr2[i]);
   }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+
 };
 
 
