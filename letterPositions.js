c81c200baeb6921c0c7bf3c11f906c41eaeb7719
vagrant <bahar.hosseini@gmail.com>
2022-07-28 17:45:28 +0000

    modifying the eqArrays function

--- letterPositions.js
+++ letterPositions.js
@@ -3,16 +3,17 @@ const assertArraysEqual = function(arr1,arr2) {
 };
 
 const eqArrays = function(arr1,arr2) {
-  if (!arr1 || !arr2) {
-    return false;
-  } else if (arr1.length !== arr2.length) {
-    return false;
-  } else if (arr1.length === 0 || arr2.length === 0) {
+  if (arr1.length !== arr2.length) {
     return false;
   }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
   return true;
-};
 
+};
 
 
 const letterPositions = function(sentence) {
