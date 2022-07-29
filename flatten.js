679800551d5cb56e238afadaf851caedaa28122a
vagrant <bahar.hosseini@gmail.com>
2022-07-29 02:28:46 +0000

    adding the flat function

--- flatten.js
+++ flatten.js
@@ -1,4 +1,15 @@
-const flatten = (array)=>{
-  return array.flat();
+const flatten = function(array) {
+  const flat = [];
+
+  array.forEach(item => {
+    if (Array.isArray(item)) {
+      flat.push(...flatten(item));
+    } else {
+      flat.push(item);
+    }
+  });
+
+  return flat;
 };
-console.log(flatten([1, 2, [3, 4], 5, [6]]));
+
+console.log(flatten([1, 2, [3, 4], 5, [6,[2,3]]]));
