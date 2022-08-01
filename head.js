0f24ff24e266f8597bbb98cb761342eecc28a67a
vagrant <bahar.hosseini@gmail.com>
2022-08-01 17:05:01 +0000

    modifying the head function

--- head.js
+++ head.js
@@ -1,27 +1,20 @@
-// Load the full build.
 const assertEqual = function(actual, expected) {
-
-  if (actual === undefined) {
-    return console.log(undefined);
-  } else if (!expected) {
-    return console.log(`❌❌❌Assertion Failed: expected is not defined`);
-  } else if (typeof actual !== typeof expected) {
-    return  console.log(`❌❌❌Assertion Failed: TypeError `);
-  } else if (actual !== expected) {
-    return console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  } else if (actual === expected) {
-    return console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  }
+  actual === expected ? console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  
 };
+
 let head = (array) =>{
+  if (array.length === 0) {
+    return undefined;
+  }
   return array[0];
 };
   
     
 // TEST CODE
-assertEqual(head([]),'s');
-assertEqual(head(['a','b']),'s');
-assertEqual(head([3,6,7]), 'hi');
+
+assertEqual(head(['a','b']),'a');
+assertEqual(head([3,6,7]), 3);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head(['a']), 'a');
 assertEqual(head([]), undefined);
